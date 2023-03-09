199287f9aed4e211e5e055824d44e42c97d400a7
KofaxTransformation <103566874+KofaxTransformation@users.noreply.github.com>
2023-03-09 17:16:31 +0100

    full documentation and script

--- OpenAI.vb
+++ OpenAI.vb
@@ -1,1 +1,167 @@
+Option Explicit
+
+' Class script: Document
+
+Private Sub Document_AfterLocate(ByVal pXDoc As CASCADELib.CscXDocument, ByVal LocatorName As String)
+   'Call the customized table locator for OpenAI
+   If InStr(LocatorName,"OpenAI") Then OpenAI_Extract(pXDoc, LocatorName)
+End Sub
+
+
+Private Sub SL_InputText_LocateAlternatives(ByVal pXDoc As CASCADELib.CscXDocument, ByVal pLocator As CASCADELib.CscXDocField)
+   'This locator outputs all the words of the document
+   Dim W As Long
+   With pLocator.Alternatives.Create
+      .Confidence=1
+      For W =0 To pXDoc.Words.Count-1
+         .Words.Append(pXDoc.Words(W))
+      Next
+   End With
+End Sub
+
+'================================
+'===========OpenAI===============
+'================================
+Private Sub OpenAI_Extract(ByVal pXDoc As CASCADELib.CscXDocument, ByVal LocatorName As String)
+   ' Read the text from the previous locator, and pass it to OpenAI. put the results into a Table
+   Dim Locs As CscLocatorDefs, OpenAILoc As CscLocatorDef, Prompt As String, Results As String
+   If pXDoc.ExtractionClass="" Then Err.Raise(4567,, "XDocument needs to be classified!")
+   Set Locs=Project.ClassByName(pXDoc.ExtractionClass).Locators
+   Set OpenAILoc=Locs.ItemByName(LocatorName)
+   If OpenAILoc.FieldType <> CscExtractionFieldType.CscFieldTypeTable Then Err.Raise(4568,,LocatorName & " needs to be a table locator")
+   If OpenAILoc.Index=0 Then Err.Raise (4569,, "There needs to be a locator previous to " & LocatorName & " to provide input")
+   With pXDoc.Locators(OpenAILoc.Index-1).Alternatives 'get output of previous locator
+      If .Count=0 Then Exit Sub ' there is no input text, so do nothing
+      Prompt=.ItemByIndex(0).Text
+   End With
+   'Call Open AI with a model, input text, temperature and max tokens. https://platform.openai.com/docs/api-reference/completions/create
+   Results=OpenAI_Cache("text-davinci-003", Prompt, pXDoc.FileName, 0, 2048)
+   ' Put the results into a table
+   With pXDoc.Locators.ItemByName(LocatorName).Alternatives(0)
+      .Confidence=1
+      Table_Parse(.Table,Results, "|")
+   End With
+End Sub
+
+Sub Table_Parse(Table As CscXDocTable, Results As String, Optional Delim As String="|")
+   'Parse the output of OpenAI into table rows and cells
+   Dim Textlines() As String, textline As String, Row As CscXDocTableRow, C As Long, CellText As String
+   Table.Rows.Clear
+   For Each textline In Split(Results,vbNewLine)
+      If InStr(textline,"|")>0 Then
+         Set Row=Table.Rows.Append
+         C=0
+         For Each CellText In Split(textline,Delim)
+            Row.Cells(C).Text=Trim(CellText)
+            C=C+1
+         Next
+      End If
+   Next
+End Sub
+
+Private Function OpenAI_Cache(Model As String, Prompt As String, Filename As String, Optional Temperature As Long=1, Optional MaxTokens As Long=2048) As String
+   'this makes a copy of the OpenAI response as a .json file next to the xdoc. Saves time and costs by not calling OpenAI when not needed.
+   'Delete the .json file if you want to call OpenAI again
+   Dim Result As String, textline As String
+   Filename=Replace(Filename,".xdc",".json")
+   If Dir(Filename)="" Then ' test for file not existing
+      Result=OpenAI_REST("text-davinci-003", Prompt, Temperature, MaxTokens)
+      Open Filename For Output As #1
+      Print #1, vbUTF8BOM & Result
+      Close #1
+   Else
+      Open Filename For Input As #1
+      Do While Not EOF(1)
+         Line Input #1, textline
+         Result = Result & textline & vbCrLf
+      Loop
+      Close #1
+   End If
+   'Extract just the text response from the results returns from OpenAI
+   OpenAI_Cache=JSON_Extract(Result,"text")
+End Function
+
+
+Private Function OpenAI_REST(Model As String, Prompt As String, Optional Temperature As Long=1, Optional MaxTokens As Long=2048) As String
+   'Call OpenAI. This returns a JSON object
+   'On Menu/Edit/References... Add reference to "Microsoft XML, v 6.0" for HTTP
+   'On Menu/Edit/References... Add reference to "Microsoft Scripting Runtime" for Dictionary
+   Dim APIKey As String, HTTP As New MSXML2.XMLHTTP60, JSON As New Dictionary
+   'See definition of temperature, maxTokens and available models here https://platform.openai.com/docs/api-reference/completions/create
+   'Get your OpenAI API key https://platform.openai.com/account/api-keys and add to script variable "OpenAI Key"
+   If Not Project.ScriptVariables.ItemExists("OpenAI Key") Then Err.Raise(4570,, "OpenAI Key needs to be in script variable 'OpenAI Key'")
+   'Create JSON request to send to OpenAI
+   JSON.Add("model", Model)
+   JSON.Add("prompt", Prompt)
+   JSON.Add("max_tokens", MaxTokens)
+   JSON.Add("temperature", Temperature)
+   'Call OpenAI
+   APIKey=Project.ScriptVariables.ItemByName("OpenAI Key").Value
+   HTTP.Open("POST", "https://api.openai.com/v1/completions",varAsync:=False)
+   HTTP.setRequestHeader("Authorization", "Bearer " & APIKey)
+   HTTP.setRequestHeader("Content-Type", "application/json")
+   HTTP.send(JSON_String(JSON))
+   If HTTP.status<>200 Then Err.Raise (4571,,"OpenAI Error: (" & HTTP.status & ") " & HTTP.responseText)
+   OpenAI_REST = HTTP.responseText
+End Function
+
+'================================
+'===========JSON=================
+'================================
+Function JSON_String(JSON As Dictionary) As String
+   'Output JSON in normal text format
+   Dim out As String, Key As String, Value
+   For Each Key In JSON.Keys
+      Select Case TypeName(JSON(Key))
+      Case "String"
+         Value="""" & JSON_Escape(JSON(Key)) & """"
+      Case "Long"
+         Value= CStr(JSON(Key))
+      Case Else
+         Value=JSON(Key)
+      End Select
+      out= out & vbTab & """" & Key & """: "  & Value & "," & vbCrLf
+   Next
+   JSON_String= "{" & vbCrLf & Left(out,Len(out)-3) & vbCrLf & "}"
+End Function
+
+Function JSON_Escape(a As String) As String
+   'https://www.json.org/json-en.html
+   a=Replace(a,"\","\\") 'backslash
+   a=Replace(a,"/","\/") 'forward slash
+   a=Replace(a,vbBack,"\b") 'backspace
+   a=Replace(a,vbFormFeed,"\f") 'form feed
+   a=Replace(a,vbNewLine,"\n") 'new line
+   a=Replace(a,vbLf,"\r") 'carraige return
+   a=Replace(a,vbTab,"\t") 'tab
+   a=Replace(a,"""","\""") 'double quote
+   JSON_Escape=a
+End Function
+
+Public Function JSON_Unescape(a As String) As String
+   'https://www.json.org/json-en.html
+   a=Replace(a,"\""","""") 'double quote
+   a=Replace(a,"\\","\") 'backslash
+   a=Replace(a,"\/","/") 'forward slash
+   a=Replace(a,"\b",vbBack) 'backspace
+   a=Replace(a,"\f",vbFormFeed) 'form feed
+   a=Replace(a,"\n",vbNewLine) 'new line
+   a=Replace(a,"\r",vbCr) 'carraige return
+   a=Replace(a,"\t",vbTab) 'tab
+   JSON_Unescape=a
+End Function
+
+Public Function JSON_Extract(JSON As String, element As String) As String
+   'simple function to return a JSON element
+   'Add reference to "Microsoft VBSCript Regular Expressions 5.5"
+   Dim rex As New RegExp, Results As VBScript_RegExp_55.MatchCollection
+   rex.Pattern="""" & element & """:\s*""(.*?)"""
+   JSON=Replace(JSON,"\""","‼")   ' convert \" to ‼ to not confuse regex on "
+   Set Results= rex.Execute(JSON)
+   If Results.Count>0 Then
+      JSON_Extract = JSON_Unescape(Replace(Results(0).SubMatches(0),"‼","\"""))
+   Else
+      Err.Raise(4789,, "failed to find " & element & " in JSON " & JSON)
+   End If
+End Function
 
--- README.md
+++ README.md
@@ -1,24 +1,71 @@
-# OpenAI & ChatGPT Integration for Kofax Transformation
-How to integration OpenAI and ChatGPT into Kofax Transformation for use in Kofax TotalAgility, Kofax RPA and Kofax Capture.  
+# OpenAI's GPT-3 Integration for Kofax Transformation
+How to integration [OpenAI](https://openai.com/)'s [GPT-3](https://openai.com/blog/gpt-3-apps) text model into Kofax Transformation for use in Kofax TotalAgility, Kofax RPA, Kofax Mobile Capture and Kofax Capture.  
 
-Note that OpenAI is a cloud-server based in USA, and the data may be used by them to train future models.
+This demo inputs this sample text to GPT-3's "most capable" model **text-davinci-003**.
+```
+    A table summarizing transfers: Please transfer forty five yen to Sonya and 85,456.34€ to Tom's bank account 6546084
+```  
+ which returns  
+```
+    | Recipient | Amount | Currency | Bank Account |
+    |-----------|--------|----------|--------------|  
+    | Sonya     | 45     | Yen      | N/A          |  
+    | Tom       | 85,456.34 | Euro    | 6546084      |
+```
+This is then put into a Kofax Transformation Table.  
+![image](https://user-images.githubusercontent.com/103566874/224074940-fed89768-e4c2-46fc-9518-4062954836d0.png)
+
+Note that OpenAI is a cloud-server based in USA, and the data may be used by them to train future models.  
+Look at the bottom of this page for sample texts and responses.
+
+## Why Integrate into Kofax Transformation?
+* Partners and customers are already asking for it.
+* Kofax Transformation is the **Intelligent Document Processing** differentiator that is the foundation of Kofax Intelligent Automation.
+![image](https://user-images.githubusercontent.com/103566874/224083618-d8820a37-e552-4707-8828-37d1263d9630.png)
+
+* Kofax Transformation is an AI platform. It provides
+  * Testing.
+  * Training and test set management.
+  * Benchmarking.
+  * Scripting.
+  * Text formatting and field validation.
+  * Integration with any other locators.
+  * Human Validation interface.
+* Supports TotalAgility, Kofax Capture, Kofax Mobile and Kofax RPA out-of-the-box.
 
 ## Register at OpenAI.com
-
-## Generate an API key
+* Create a free account at https://platform.openai.com/signup.
+* OpenAI's GPT-3 offers [**chat**](https://platform.openai.com/docs/guides/chat) as in ChatGPT and [**text completions**](https://platform.openai.com/docs/guides/completion). We will use text completions in this demo, because we are giving an input and want an output, we are not making a dialog.
+* Open [**text completions**](https://platform.openai.com/docs/guides/completion) and experiment (see down the bottom for examples).
+* You have access numerous [models](https://platform.openai.com/docs/models). This demo uses GPT-3's **most capable** model **text-davinci-003**.
+## Generate an OpenAI API key
 * Open https://platform.openai.com/account/api-keys
 * Click **+ Create new secret key**.
 * Copy the secret key to paste into Kofax Transformation.
 
 ## Configure Kofax Transformation
+* Download the [OpenAI] project from GitHub.  
+* Open **Menu Project/Configuration/ScriptVariables** and paste your key into script variable **OpenAI Key**.
+![image](https://user-images.githubusercontent.com/103566874/223722190-2225522f-e6fe-42db-8f74-035e6170ef79.png)
+* Add script locator called **SL_InputText**. *This script locator just reads in the text of the sample document*.
+* Create 5 new Columns in the Column Pool in **ProjectSettings/Tables**. This allows you to use a generic table to test various table formats.  
+![image](https://user-images.githubusercontent.com/103566874/224077622-a8a3e65b-4a5e-4b33-8afc-bbacc32e4671.png)
+* Create a Table model called **Generic** with the 5 columns.  
+![image](https://user-images.githubusercontent.com/103566874/224077952-43c2154c-013c-4cbe-9c4d-91a69eef2528.png)
 
-* Add a variable OpenAI-key.
-* Add script locator, starting with "OpenAI_".
-* Add the key value pairs that you expect
 * Add the [script](OpenAI.vb) to the document class. The script uses the OpenAI [REST API](https://platform.openai.com/docs/api-reference/introduction).
-* Press Test on the Locator.
+* AIn Script Editor Menu **Edit/References...** add references to
+    * Microsoft XML, v 6.0
+    * Microsoft Scripting Runtime  
+    * Microsoft VBSCript Regular Expressions 5.5  
+    ![image](https://user-images.githubusercontent.com/103566874/224080390-e928a33f-dcb5-468b-a1cd-96928bbbf465.png)
+ 
+* Press Classify & Extract (F7) on the sample document.
+* View results in **Extraction Results** Window.  
+![image](https://user-images.githubusercontent.com/103566874/224080851-7446be1d-8866-4ce7-8b25-9a3b6674ea8b.png)
 
-Let me know any useful examples you find.
+
+Let me know of any useful examples you find.
 
 ## Examples
 ```
@@ -49,10 +96,10 @@ Bank and Branch Code: 4832                     <--- Note that this is incorrect!
 Account Number: 000012345864
 ```
 ```
-please parse "please transfer forty thousand euros to Peter Brown's bank account AT483200000012345864"
+please parse "please transfer forty thousand euros to Peter Brooks' bank account AT483200000012345864"
 
 Request: Transfer forty thousand euros (€40,000)
-Payee: Peter Brown 
+Payee: Peter Brooks 
 Account Number: AT483200000012345864
 ```
 ```
